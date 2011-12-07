26b3186e9375eaa295637b020a3f61d70112afbc
Marcelo Criscuolo <criscuolo.marcelo@gmail.com>
2011-12-06 23:06:38 -0200

    first import

---
+++ set.js
@@ -0,0 +1,48 @@
+
+//TODO MIT license notice
+
+function Set(elements) {
+	this.bag = [];
+	var i;
+
+	if (typeof elements != "undefined") { // optional param
+		for (i=0; i < elements.length; i++) {
+			this.add(elements[i]);
+		}
+	}
+}
+
+Set.prototype.search = function(e) {
+	//TODO document this
+	var i = 0;
+	var j = this.bag.length;
+	var pivot;
+
+	while (i < j) {
+		pivot = i + Math.floor((j - i) / 2);
+		if (this.bag[pivot] == e) {
+			return pivot;
+		}
+
+		if (e > this.bag[pivot]) {
+			i = pivot + 1;
+		} else {
+			j = pivot;
+		}
+	}
+
+	return i;
+}
+
+Set.prototype.add = function(e) {
+	var p = this.search(e);
+	if (this.bag[p] != e) {
+		this.bag.splice(p, 0, e); // insert e at position p
+	}
+}
+
+Set.prototype.contains = function(e) {
+	var p = this.search(e);
+	return (this.bag[p] == e);
+}
+
