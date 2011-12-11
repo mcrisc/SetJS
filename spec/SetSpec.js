156029f31d9cf5161bba3992a9bb511ed9a092ce
Marcelo Criscuolo <criscuolo.marcelo@gmail.com>
2011-12-11 21:55:50 -0200

    first working version

--- spec/SetSpec.js
+++ spec/SetSpec.js
@@ -1,5 +1,5 @@
 function equalArrays(a1, a2, ignoreOrder) {
-	if (a1.length != a2.length) {return false;} // avoids unnecessary sorting
+	if (a1.length != a2.length) {return false;}
 
 	function comparator(a, b) {
 		return a - b;
@@ -17,7 +17,7 @@ function equalArrays(a1, a2, ignoreOrder) {
 	return true;
 }
 
-describe("Set element storage", function() {
+describe("Set", function() {
 	var s;
 	beforeEach(function() {
 		s = new Set([32, 90, 32, 5, 15]);
@@ -27,10 +27,64 @@ describe("Set element storage", function() {
 		expect(equalArrays(s.bag_, [32, 90, 5, 15], true)).toBe(true);
 	});
 
-	it("should store elements ordered", function() {
+	it("should store elements in their natural order", function() {
 		expect(equalArrays(s.bag_, [5, 15, 32, 90], false)).toBe(true);
+	});
+
+	it("should have a 'contains' operation", function() {
+		expect(s.contains(90)).toBe(true);
+		expect(s.contains(99)).toBe(false);
+	});
+
+	it("should keep the set ordered when adding a new element", function() {
+		s.add(17);
+		expect(equalArrays(s.bag_, [5, 15, 17, 32, 90], false)).toBe(true);
+	});
+
+	it("should find an element's position", function() {
+		expect(s.search(15)).toBe(1);
+	});
+
+	it("should find the position where an element should be", function() {
+		expect(s.search(17)).toBe(2);
+	});
+
+	it("should be able to tell its size", function() {
+		var t = new Set();
+		expect(t.size()).toBe(0);
+		expect(s.size()).toBe(4);
 	});
 
 });
 
+// search and contains
+describe("Operation: difference (a - b)", function() {
+	var s1, s2;
+	beforeEach(function() {
+		s1 = new Set([1, 2, 13, 15, 19, 20, 23, 26, 28, 32]);
+		s2 = new Set([5, 15, 16, 20, 25]);
+	});
+
+	it('should work when |a| > |b|', function() {
+		var a = s1; var b = s2;
+		var resultSet = a.difference(b);
+		expect(equalArrays(resultSet.bag_, [1, 2, 13, 19, 23, 26, 28, 32], false)).toBe(true);
+	});
+
+	it('should work when |a| < |b|', function() {
+		var a = s2; var b = s1;
+		var resultSet = a.difference(b);
+		expect(equalArrays(resultSet.bag_, [5, 16, 25], false)).toBe(true);
+	});
+
+	it('should return a copy of a, when b is empty', function() {
+		var a = s1; var b = new Set();
+		expect(equalArrays(a.difference(b).bag_, a.bag_, false)).toBe(true);
+	});
+
+	it('should return the empty set, when a is empty', function() {
+		var a = new Set(); var b = s1;
+		expect(equalArrays(a.difference(b).bag_, a.bag_, false)).toBe(true);
+	});
+});
 
--- src/Set.js
+++ src/Set.js
@@ -12,11 +12,11 @@ function Set(elements) {
 	}
 }
 
-Set.prototype.search = function(e) {
+Set.prototype.search = function(e, start) {
 	//TODO document this
-	var i = 0;
 	var j = this.bag_.length;
 	var pivot;
+	var i = arguments.length == 2 ? start : 0;
 
 	while (i < j) {
 		pivot = i + Math.floor((j - i) / 2);
@@ -44,5 +44,34 @@ Set.prototype.add = function(e) {
 Set.prototype.contains = function(e) {
 	var p = this.search(e);
 	return (this.bag_[p] == e);
+}
+
+Set.prototype.size = function() {
+	return this.bag_.length;
+}
+
+Set.prototype.difference = function(otherSet) {
+	var result = new Set();
+	if (this.size() == 0) {return result;}
+
+	var i;
+	var j = 0;
+	var a = this.bag_; var b = otherSet.bag_;
+	for (i=0; i < a.length; i++) {
+		if (a[i] > b[j]) {
+			j = otherSet.search(a[i], j); // finds First b[j] Not Lesser than a[i]
+		}
+
+		if (j == b.length) { // end of b
+			result.bag_ = result.bag_.concat(a.slice(i));
+			break;
+		}
+
+		if (a[i] < b[j]) {
+			result.bag_.push(a[i]);
+		}
+	}
+	
+	return result;
 }
 
